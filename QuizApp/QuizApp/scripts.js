b84c0bcadd9870b8e8fc0756ec2dd1224e5c2c06
Bforsyth1234 <brooks.forsyth@gmail.com>
2014-08-23 08:30:49 -0400

    testing

---
+++ QuizApp/QuizApp/scripts.js
@@ -0,0 +1,18 @@
+﻿var app = {};
+
+app.quiz = [];
+app.info = [test];
+
+app.quiz - function () {
+    this.question = question;
+    this.answer = answer;
+};
+
+app.output = function () {
+    var holder = "";
+
+    holder += "<p>";
+    holder += "Question: " +  app.info;
+    holder += "</p>";
+
+};
