9361e0da02ad7dcc1ff03317f69d0d133d526ce6
Bforsyth1234 <brooks.forsyth@gmail.com>
2014-08-23 08:31:58 -0400

    1

---
+++ day5/day5/script.js
@@ -0,0 +1,9 @@
+﻿var Person = function (name) {
+    this.name = name;
+    this.eyeColor = eyeColor;
+}
+
+
+var people = [];
+people.push(new Person("Brooks", "blue"));
+people.push(new Person("Nasser", "brown"));
